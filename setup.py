ab8d3c32379fe3c1af132ae310d8a97230a20cea
AntonelloManenti <antonellomanenti@gmail.com>
2022-01-14 01:53:28 +0100

    last

--- setup.py
+++ setup.py
@@ -15,9 +15,9 @@ setup(
         author_email="antonellomanenti@gmail.com",
         license="MIT",
         classifiers=[
-            "Development Status :: 1 - Planning",
+            # "Development Status :: 1 - Planning",
             # "Development Status :: 2 - Pre-Alpha",
-            # "Development Status :: 3 - Alpha",
+            "Development Status :: 3 - Alpha",
             # "Development Status :: 4 - Beta",
             # "Development Status :: 5 - Production/Stable",
             # "Development Status :: 6 - Mature",
