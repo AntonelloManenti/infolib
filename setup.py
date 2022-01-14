8fafa6dbd95a9909e90d7819d3629893abedfeab
AntonelloManenti <antonellomanenti@gmail.com>
2022-01-14 11:59:10 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ else:
 
 setup(
         name="infolib",
-        version="0.1.19",
+        version="0.1.20",
         packages=find_packages(),
         description="A small, simple and sturdy library to overview our PandasDataframe",
         long_description=long_description,
