3462450f472971fefc21e06c3a9d4e1eccece30a
AntonelloManenti <antonellomanenti@gmail.com>
2022-01-16 13:10:17 +0100

    New for Pypi

--- setup.py
+++ setup.py
@@ -47,6 +47,8 @@ setup(
             "psutil",
             "regex",
             ],
+        package_dir={"": "src"},
+        packages=setuptools.find_packages(where="src"),
         python_requires='>3.6',
         entry_points="""
         [console_scripts]
